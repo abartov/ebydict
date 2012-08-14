6f7915c584c0cb1cba6bbdd7d016258986fd2b43
Asaf Bartov <asaf.bartov@gmail.com>
2012-08-13 17:52:32 -0700

    implemented reproofing and publishing

--- app/controllers/definition_controller.rb
+++ app/controllers/definition_controller.rb
@@ -6,11 +6,23 @@ class DefinitionController < ApplicationController
   end
 
   def review
+    @d = EbyDef.find(params[:id])
   end
 
   def publish
+    @d = EbyDef.find(params[:id])
+    @d.status = 'Published'
+    @d.save
+    flash[:notice] = t(:definition_published_html, :defhead => @d.defhead).html_safe
+
+    redirect_to :action => 'list'
   end
 
   def reproof
+    @d = EbyDef.find(params[:id])
+    @d.status = 'NeedProof'
+    @d.save
+    flash[:notice] = t(:definition_sent_to_reproof_html, :defhead => @d.defhead).html_safe
+    redirect_to :action => 'list'
   end
 end
