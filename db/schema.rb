eb8fce1dac6988f89a1ca31859c1456d3367de09
Asaf Bartov <asaf.bartov@gmail.com>
2012-12-13 21:23:39 +0200

    enlarge session store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121206022332) do
+ActiveRecord::Schema.define(:version => 20121213192212) do
 
   create_table "eby_column_images", :force => true do |t|
     t.integer  "eby_scan_image_id"
@@ -102,10 +102,10 @@ ActiveRecord::Schema.define(:version => 20121206022332) do
   end
 
   create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "session_id",                     :null => false
+    t.text     "data",       :limit => 16777215
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
