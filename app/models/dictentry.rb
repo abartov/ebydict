318e9cbb5cf7a86ffb9248c4d41fee540597b233
Asaf Bartov <asaf.bartov@gmail.com>
2013-07-02 16:53:19 -0700

    removed old useless files

--- app/models/dictentry.rb
+++
@@ -1,6 +0,0 @@
-class Dictentry < ActiveRecord::Base
-	belongs_to :proofer1, :class_name => "User", :foreign_key => "proofer1_id"
-	belongs_to :proofer2, :class_name => "User", :foreign_key => "proofer2_id"
-	belongs_to :proofer3, :class_name => "User", :foreign_key => "proofer3_id"
-	has_many :entryimages
-end
--- app/models/scanimage.rb
+++
@@ -1,5 +0,0 @@
-class Scanimage < ActiveRecord::Base
-  has_many :entryimages
-  belongs_to :assignee, :class_name => 'EbyUser'
-  belongs_to :partitioner, :class_name => 'EbyUser'
-end
