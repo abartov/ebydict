171ac34ec46c47135749c64fda92f99262e1ed2a
Asaf Bartov <asaf.bartov@gmail.com>
2014-02-17 00:15:32 -0800

    beginning of functional test (not really useful yet)

--- test/functional/definition_controller_test.rb
+++ test/functional/definition_controller_test.rb
@@ -3,7 +3,11 @@ require 'test_helper'
 class DefinitionControllerTest < ActionController::TestCase
   test "should get list" do
     get :list
-    assert_response :success
+    #assert_response :success
+    assert_redirected_to(controller: :login, action: :login) # when not logged in
+    u = users(:admin)
+    # TODO: login
+    # test actual list view
   end
 
   test "should get review" do
@@ -20,5 +24,8 @@ class DefinitionControllerTest < ActionController::TestCase
     get :reproof
     assert_response :success
   end
-
+  test "should get view" do
+    get :view, {:id => eby_defs(:one).id}
+    assert_response :success
+  end
 end
